56ba8f51a7a9a299e06bad75e06dc5b55c4741da
Robin Wang <510471194@qq.com>
2020-11-29 19:21:26 +0800

    improved comments fro ThreadLocal

--- src/main/java/com/legend/juc/c_022_RefTypeAndThreadLocal/ThreadLocal2.java
+++ src/main/java/com/legend/juc/c_022_RefTypeAndThreadLocal/ThreadLocal2.java
@@ -27,7 +27,7 @@ public class ThreadLocal2 {
 				e.printStackTrace();
 			}
 			
-			System.out.println(tl.get());
+			System.out.println("t1:" + tl.get());//这里打印出来的是null是因为ThreadLocal在线程间是相互独立的
 		}).start();
 		
 		new Thread(()->{
@@ -37,6 +37,14 @@ public class ThreadLocal2 {
 				e.printStackTrace();
 			}
 			tl.set(new Person());
+
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("t2: " + tl.get());//这里可以get到，因为是因为在本线程内
+
 		}).start();
 	}
 	
